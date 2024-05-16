43bc6097ade9701418fba52569a39f85e4e2aaf8
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-16 21:32:13 +0300

    add test script

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,7 +1,29 @@
 package com.company;
 
+import com.company.service.UserService;
+import com.company.service.UserServiceJDBCImpl;
+
 public class Main {
     public static void main(String[] args) {
+        script(new UserServiceJDBCImpl());
+    }
 
+    public static void script(UserService userService) {
+        userService.createUserTable();
+        userService.saveUser("Abc0", "Bcd", (byte) 35);
+        userService.saveUser("Abc1", "Bcd", (byte) 45);
+        userService.saveUser("Abc2", "Bcd", (byte) 55);
+        userService.saveUser("Abc3", "Bcd", (byte) 65);
+
+        System.out.println();
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.removeUserById(2);
+        System.out.println();
+        userService.getAllUsers().forEach(System.out::println);
+
+        userService.clearUserTable();
+        System.out.println();
+        userService.getAllUsers().forEach(System.out::println);
     }
 }
