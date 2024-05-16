0962cf51b9eebb7405535fccb6a353575c3144c2
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-16 16:18:31 +0300

    add annotations to User class

--- src/main/java/com/company/bean/User.java
+++ src/main/java/com/company/bean/User.java
@@ -1,5 +1,14 @@
 package com.company.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private long id;
     private String name;
