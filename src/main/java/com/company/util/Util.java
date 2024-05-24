5459779582d7483c3daca098c46496b1c7006b41
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-24 13:53:06 +0300

    add Hibernate connection method

--- src/main/java/com/company/util/Util.java
+++ src/main/java/com/company/util/Util.java
@@ -1,11 +1,19 @@
 package com.company.util;
 
+import com.company.bean.User;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Properties;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Util {
@@ -13,6 +21,8 @@ public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/test";
     private static final String USER_NAME = "root";
     private static final String PASSWORD = "root";
+
+    private static SessionFactory sessionFactory;
 
     public static Connection getJDBCConnection() {
         Connection connection;
@@ -27,6 +37,30 @@ public class Util {
 
         return connection;
     }
+
+    public static SessionFactory getHibernateConnection() {
+        if (Objects.isNull(sessionFactory)) {
+            Configuration configuration = new Configuration();
+            Properties properties = new Properties();
+            properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            properties.put(Environment.URL, URL);
+            properties.put(Environment.USER, USER_NAME);
+            properties.put(Environment.PASS, PASSWORD);
+            properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+            properties.put(Environment.SHOW_SQL, "true");
+            properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            properties.put(Environment.HBM2DDL_AUTO, "create");
+            configuration.setProperties(properties);
+            configuration.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        }
+
+        return sessionFactory;
+    }
+
 
 
 }
