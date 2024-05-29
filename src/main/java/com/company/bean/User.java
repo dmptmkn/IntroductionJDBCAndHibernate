59b49ecb9c8daa0a7622b9636ac1b2d360e40540
dmptmkn <dm.ptmkn@yandex.ru>
2024-05-29 14:08:12 +0300

    add User class minor implementation updates

--- src/main/java/com/company/bean/User.java
+++ src/main/java/com/company/bean/User.java
@@ -3,12 +3,13 @@ package com.company.bean;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Setter
 @ToString
+@RequiredArgsConstructor
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "user")
 public class User {
@@ -25,4 +26,23 @@ public class User {
 
     @Column
     private byte age;
+
+    public User(String name, String lastName, byte age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id && age == user.age && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, lastName, age);
+    }
 }
